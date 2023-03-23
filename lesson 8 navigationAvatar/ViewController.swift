5f56589601f2fb9d52edf5843dbe821856cb60aa
LBkos <constantin.l.o.p@gmail.com>
2023-03-23 09:15:48 +0700

    Initial Commit

---
+++ lesson 8 navigationAvatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 8 navigationAvatar
+//
+//  Created by Константин Лопаткин on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
