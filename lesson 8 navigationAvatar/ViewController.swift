e43a0ed6502407039cac20f14a1fbdf97cb0ac9c
LBkos <constantin.l.o.p@gmail.com>
2023-03-23 11:37:38 +0700

    add avatar

--- lesson 8 navigationAvatar/ViewController.swift
+++ lesson 8 navigationAvatar/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageView: UIImageView!
+    var scrollView = UIScrollView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
+        imageView.tintColor = .systemGray3
+            
+        view.backgroundColor = .white
+        
+        scrollView.contentSize = .init(width: view.bounds.width, height: 2000)
+        scrollView.clipsToBounds = true
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        setNavigationView()
+
+        view.addSubview(scrollView)
+        scrollView.frame = .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        
     }
+    
+    func setNavigationView() {
+        
+        guard let navigationBar = navigationController?.navigationBar else {
+            return
+        }
+        title = "Avatar"
+        
+        guard let UINavigationBarLargeTitleView = NSClassFromString("_UINavigationBarLargeTitleView") else { return }
 
+        navigationBar.subviews.forEach { subview in
+            if subview.isKind(of: UINavigationBarLargeTitleView.self) {
+                
+                subview.addSubview(imageView)
+            }
+        }
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor,
+                                             constant: -16),
+            imageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor,
+                                              constant: -12),
+            imageView.heightAnchor.constraint(equalToConstant: 36),
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
+        ])
 
+    }
 }
-
